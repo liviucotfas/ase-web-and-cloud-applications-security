1c828374ddce4c4a7170bcb6d140e04d6283d225
Liviu-Adrian Cotfas <lcotfas@gmail.com>
2021-05-16 23:22:13 +0300

    Various changes

---
+++ 1 - HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+    }
+}
