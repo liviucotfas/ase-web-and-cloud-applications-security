db325363563d93ef3f4c0948bb66531b806e02f2
Liviu-Adrian Cotfas <lcotfas@gmail.com>
2024-02-25 01:06:19 +0200

    Updated XSS Sample

--- 11 - Vulnerabilities - CORS (Cross-Origin Resource Sharing)/InfectedWebsite/Program.cs
+++ 11 - Vulnerabilities - CORS (Cross-Origin Resource Sharing)/InfectedWebsite/Program.cs
@@ -7,8 +7,8 @@ namespace InfectedWebsite
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.UseStaticFiles();
-            app.MapGet("/", () => "Hello World!");
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
 
             app.Run();
         }
