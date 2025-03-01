c724e9822c5c1be0951bd35f61ea15191995d405
Liviu-Adrian Cotfas <lcotfas@gmail.com>
2025-03-01 23:06:39 +0200

    Revert "Updated sample to .net 8"
    This reverts commit 19eebcf38e8ddbd4904dcdc5d35f3a988276bf0d.

---
+++ 02 - HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CourseInvites.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CourseInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error 
+                return View();
+            }
+        }
+
+        public IActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ 02 - HelloWorld/Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseInvites.Models
+{
+	public static class Repository
+	{
+		private static List<GuestResponse> responses = new List<GuestResponse>();
+		public static IEnumerable<GuestResponse> Responses
+		{
+			get
+			{
+				return responses;
+			}
+		}
+		public static void AddResponse(GuestResponse response)
+		{
+			responses.Add(response);
+		}
+	}
+}
